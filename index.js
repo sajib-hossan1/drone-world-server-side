f72d65fcd9f48fdf928ccf2f62628d941a7e7e81
sajib-hossan1 <sajib.hossan112@gmail.com>
2021-11-12 13:26:22 +0600

    connected with database

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cowhf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('data base connected');
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
