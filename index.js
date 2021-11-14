06026823c45bbfa27f2a095e887f111dbb3505b5
sajib-hossan1 <sajib.hossan112@gmail.com>
2021-11-14 17:21:33 +0600

    get orders and delete an order api created

--- index.js
+++ index.js
@@ -22,12 +22,52 @@ async function run() {
 
         const database = client.db("drone-world");
         const productsCollection = database.collection("products")
+        const orderCollection = database.collection("orders")
 
+        // get all products
         app.get('/products', async (req,res) => {
             const cursor = productsCollection.find({})
             const products = await cursor.toArray();
             res.send(products)
         })
+
+        // get a single item 
+        app.get('/buyNow/:id', async (req, res) => {
+          const id = req.params.id ;
+          const query = {_id : ObjectId(id)};
+          const result = await productsCollection.findOne(query);
+          res.send(result)
+      })
+
+      // add orders api
+      app.post('/order', async (req,res) => {
+        const order = req.body;
+        const result = await orderCollection.insertOne(order);
+        res.json(result);
+      })
+
+      // get orders for an user
+      app.get('/orders', async(req, res) => {
+        const email = req.query.email;
+        const query = { email : email };
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders)
+      })
+
+
+      // delete an order for an user
+      app.delete('/orders/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) }
+        const result = await orderCollection.deleteOne(query)
+        console.log('delete user', result)
+        res.json(result)
+    })
+
+    
+
+
 
     }
     finally {
